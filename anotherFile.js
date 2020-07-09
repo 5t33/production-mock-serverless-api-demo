915a4ebe98a2c44863701b8a2491cdf3ba1730be
5t33 <stp.staley@gmail.com>
2020-07-09 00:44:43 -0400

    remove subseg arg from anotherFile

--- anotherFile.js
+++ anotherFile.js
@@ -1,16 +1,16 @@
 const AWSXRay = require('aws-xray-sdk-core')
 const { ApiError } = require('./utils');
 
-module.exports.functionThatRejectsWith400 = (segment) => 
+module.exports.functionThatRejectsWith400 = () => 
   AWSXRay.captureFunc.captureAsyncFunc("functionThatRejectsWith400", async subseg => {
     subseg.close();
     return Promise.reject(new ApiError(400, [
       "wow you messed up, huh?", 
       "Looks like somebody didn't read the documentation."
     ]))
-  },segment);
+  });
 
-module.exports.functionThatRejects = (segment) => 
+module.exports.functionThatRejects = () => 
   AWSXRay.captureAsyncFunc("functionThatRejects", async subseg => {
     try {
       throw new Error("Oh No");
@@ -21,10 +21,10 @@ module.exports.functionThatRejects = (segment) =>
     } finally {
       subseg.close();
     }
-  }, segment);
+  });
 
-module.exports.functionThatResolves = (segment) => 
+module.exports.functionThatResolves = () => 
   AWSXRay.captureFunc.captureAsyncFunc("functionThatRejects", async subseg => {
     subseg.close();
     Promise.resolve({so: "much", data: "wow"})
-  }, segment);
+  });
